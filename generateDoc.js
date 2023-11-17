942769ddc598277b573da1471d667d9509cca589
mkslanc <mkslanc@gmail.com>
2023-11-17 17:37:49 +0400

    remove unneccessary modules; update typedoc

--- generateDoc.js
+++ generateDoc.js
@@ -1,31 +1,15 @@
-var acegitlink = require("./AceGitLinkPlugin").AceGitLinkPlugin;
-var borrowsTag = require("./BorrowsTagPlugin").BorrowsTagPlugin;
 var typedoc = require('typedoc');
-const fs = require("fs");
-var options = JSON.parse(fs.readFileSync("generator-options.json", "utf8").toString());
 
-var settings = {
-    "name": options["title"],
-    "readme": options["readme"],
-    "entryPoints": options["entryPoints"]
-};
+async function main() {
+    const app = await typedoc.Application.bootstrapWithPlugins();
+    var gitLink = "https://github.com/ajaxorg/ace/tree/" + process.env.ACE_VERSION + "/{path}#{line}";
+    app.options.setValue("sourceLinkTemplate", gitLink);
 
-var app = new typedoc.Application();
-app.bootstrap(settings);
-app.options.setCompilerOptions(options["entryPoints"], {
-    "module": "commonjs",
-    "target": "es5",
-    "sourceMap": false,
-    "ignoreCompilerErrors": true
-})
-app.options.addDeclaration({name: 'acegitlink'});
-app.converter.addComponent('acegitlink', acegitlink);
-app.renderer.addComponent('borrowstag', borrowsTag);
-var gitLink = options["gitLink"];
-if (gitLink.charAt(gitLink.length - 1) !== '/') gitLink += '/';
-gitLink += process.env.ACE_VERSION + "/";
+    const projectReflection = await app.convert();
 
-app.options.setValue("acegitlink", gitLink);
-var projectReflection = app.convert();
+    if (projectReflection) {
+        await app.generateDocs(projectReflection, process.argv[2] || "doc");
+    }
+}
 
-app.generateDocs(projectReflection, process.argv[2]);
+main();
