fa8f9c2cb5198818de14479c143c49c2d6404b68
Marin Sokol <sokomari@amazon.com>
2024-10-23 15:27:20 +0200

    opening code source links in the same window

--- generateDoc.js
+++ generateDoc.js
@@ -1,9 +1,10 @@
 var typedoc = require('typedoc');
 
 async function main() {
-    const app = await typedoc.Application.bootstrapWithPlugins();
-    var gitLink = "https://github.com/ajaxorg/ace/tree/" + process.env.ACE_VERSION + "/{path}#L{line}";
-    app.options.setValue("sourceLinkTemplate", gitLink);
+    const app = await typedoc.Application.bootstrapWithPlugins({
+        sourceLinkExternal: false,
+        sourceLinkTemplate: "https://github.com/ajaxorg/ace/tree/" + process.env.ACE_VERSION + "/{path}#L{line}",
+    });
 
     const projectReflection = await app.convert();
 
